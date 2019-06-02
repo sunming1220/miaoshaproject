18a4b5489e722b9ea22ca4940520edfc50437afe
sunming <1984206501@qq.com>
2019-06-02 14:41:55 +0800

    通过id得到user对象

--- src/main/java/com/example/miaoshaproject/controller/UserController.java
+++ src/main/java/com/example/miaoshaproject/controller/UserController.java
@@ -1,17 +1,26 @@
 package com.example.miaoshaproject.controller;
 
+import com.example.miaoshaproject.service.UserService;
+import com.example.miaoshaproject.service.model.UserModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller("user")
 @RequestMapping("/user")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
     @RequestMapping("/get")
-    public void getUser(@RequestParam("id") Integer id){
+    @ResponseBody
+    public UserModel getUser(@RequestParam("id") Integer id){
 
         //调用service服务获取对应ID的用户对象并返回给前端
-
+        UserModel userModel = userService.getUserById(id);
+        return userModel;
     }
 }
